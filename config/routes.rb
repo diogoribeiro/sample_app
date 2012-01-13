e400b3d63d449720c49ff4a5d7ae963c4d57be47
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2012-01-13 03:20:35 -0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  root :to=> 'pages#home'
 
-  get "pages/contact"
+  match '/contact', :to=> 'pages#contact'
+  match '/about', :to=> 'pages#about'
+  match '/help', :to=> 'pages#help'
 
-  get "pages/about"
+  match '/signup', :to=> 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -39,4 +39,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successfull" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => ' | Help')
+    end
+  end
+
 end
